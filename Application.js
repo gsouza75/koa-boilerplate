1cb1b7429d614fe31611396a09822e312189b914
Gustavo Souza <gsouza75@gmail.com>
2014-10-13 11:53:13 -0700

    Skeleton App.

--- Application.js
+++ Application.js
@@ -1,11 +1,22 @@
 'use strict';
 
-let config = require('./config'),
-	middleware = require('./middleware');
+let
+	compress = require('koa-compress'), 
+	config = require('./config'),
+	controller = require('./controllers'),
+	koa = require('koa'),
+	logger = require('koa-logger'),
+	middleware = require('./middleware'),
+	path = require('path'),
+	route = require('koa-route'),
+	static = require('koa-static');
 
 
 function App() {}
 
+App.prototype.setMiddleware = function () {};
+
+App.prototype.start = function () {};
 
 
-module.exports = new App();
+module.exports = App;
