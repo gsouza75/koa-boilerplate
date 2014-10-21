f8aba0c8ce964222236516fe94a3992173217b68
Gustavo Souza <gsouza75@gmail.com>
2014-10-21 15:04:21 -0700

    Cleanup.

--- helpers/ErrorView.js
+++ helpers/ErrorView.js
@@ -1,20 +1,17 @@
 'use strict';
 
-let 
-  _ = require('lodash'),
-  View = require('./View');
+let View = require('./View');
 
 
-function ErrorView(template, error, options) {
-	View.apply(this, arguments);
+function ErrorView(template, error) {
+  View.apply(this, arguments);
 
-	let errorStatus = error.status;
-	if (errorStatus) {
-		this.status = errorStatus;
-	}
+  let errorStatus = error.status;
+  if (errorStatus) {
+    this.status = errorStatus;
+  }
 
-	// this.setStatus();
-	this.content = { message: error.message, status: this.status };
+  this.content = { message: error.message, status: this.status };
 }
 
 ErrorView.prototype = Object.create(View.prototype);
@@ -22,8 +19,8 @@ ErrorView.prototype = Object.create(View.prototype);
 ErrorView.prototype.constructor = ErrorView;
 
 ErrorView.prototype.defaults = {
-	status: 500,
-	template: 'error'
+  status: 500,
+  template: 'error'
 };
 
 
