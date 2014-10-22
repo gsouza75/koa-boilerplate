815e24ccc7649cb16049c9ab0c958c2a73ad90b1
Gustavo Souza <gsouza75@gmail.com>
2014-10-21 17:33:53 -0700

    Convert to space indentation.

--- Application.js
+++ Application.js
@@ -1,72 +1,72 @@
 'use strict';
 
 let
-	compress = require('koa-compress'),
-	config = require('./config'),
-	koa = require('koa'),
-	logger = require('koa-logger'),
-	middlewares = require('./middlewares'),
-	route = require('koa-route'),
-	routes = require('./routes'),
-	serve = require('koa-static'),
-	View = require('./helpers/View');
+  compress = require('koa-compress'),
+  config = require('./config'),
+  koa = require('koa'),
+  logger = require('koa-logger'),
+  middlewares = require('./middlewares'),
+  route = require('koa-route'),
+  routes = require('./routes'),
+  serve = require('koa-static'),
+  View = require('./helpers/View');
 
 
 function App() {
-	this.koaApp = koa();
-	this.config = config;
-	this.routes = routes;
-	this.applyMiddlewares();
+  this.koaApp = koa();
+  this.config = config;
+  this.routes = routes;
+  this.applyMiddlewares();
 }
 
 App.prototype.applyMiddlewares = function () {
-	this.koaApp.use(logger());
-	this.applyCustomMiddlewares();
-	this.applyRouteHandlers();
-	this.koaApp.use(serve(this.config.staticDir));
-	this.koaApp.use(compress());
+  this.koaApp.use(logger());
+  this.applyCustomMiddlewares();
+  this.applyRouteHandlers();
+  this.koaApp.use(serve(this.config.staticDir));
+  this.koaApp.use(compress());
 };
 
 App.prototype.applyCustomMiddlewares = function () {
-	for (let middleware in middlewares) {
-		this.koaApp.use(middlewares[middleware]);
-	}
+  for (let middleware in middlewares) {
+    this.koaApp.use(middlewares[middleware]);
+  }
 };
 
 App.prototype.applyRouteHandlers = function () {
-	let self = this;
+  let self = this;
 
-	for (let verb in this.routes) {
-		let handlers = this.routes[verb];
+  for (let verb in this.routes) {
+    let handlers = this.routes[verb];
 
-		for (let urlPath in handlers) {
-			let handler = handlers[urlPath];
+    for (let urlPath in handlers) {
+      let handler = handlers[urlPath];
 
-			this.koaApp.use(route[verb](urlPath, function *() {
-				let returnValue = handler.apply(this, arguments);
+      this.koaApp.use(route[verb](urlPath, function *() {
+        let returnValue = handler.apply(this, arguments);
 
-				if (returnValue instanceof View) {
-					returnValue = returnValue
-						.setContext(this)
-						.setStatus()
-						.setMediaType()
-						.render();
-				}
+        if (returnValue instanceof View) {
+          returnValue = returnValue
+            .setContext(this)
+            .setStatus()
+            .setMediaType()
+            .render();
+        }
 
-				this.body = typeof returnValue === 'string' ?
-					returnValue :
-					yield returnValue;
-			}));
-		}
-	}
+        this.body = typeof returnValue === 'string' ?
+          returnValue :
+          yield returnValue;
+      }));
+    }
+  }
 };
 
 App.prototype.start = function () {
-	let config = this.config,
-		port = config.port;
+  let config = this.config,
+    port = config.port;
 
-	console.log('Starting app %s with config:', config.name);
-	console.dir(config);
+  console.log('Starting app %s with config:', config.name);
+  console.dir(config);
 
   this.koaApp.listen(port);
 
