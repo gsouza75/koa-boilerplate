80a34b77ea76fbecb4ffbb2d1e7b28ed6dfa5dee
Gustavo Souza <gsouza75@gmail.com>
2014-10-21 17:24:06 -0700

    Convert indent to spaces.

--- config/index.js
+++ config/index.js
@@ -1,51 +1,51 @@
 'use strict';
 
 let 
-	_ = require('lodash'),
-	path = require('path'),
-	viewEngine = require('co-views');
+  _ = require('lodash'),
+  path = require('path'),
+  viewEngine = require('co-views');
 
 
 function Config() {
-	_.defaults(this, this.defaults);
-	this.applyOverride();
+  _.defaults(this, this.defaults);
+  this.applyOverride();
 }
 
 Config.prototype.defaults = {
-	name: 'koa-boilerplate-app',
+  name: 'koa-boilerplate-app',
 
-	port: 3000,
+  port: 3000,
 
-	staticDir: path.join(__dirname, '..', 'public'),
+  staticDir: path.join(__dirname, '..', 'public'),
 
-	template: function () {
-		return viewEngine('views', {
-		  map: {
-		    html: 'swig',
-		    md: 'hogan'
-		  }
-		});
-	}
+  template: function () {
+    return viewEngine('views', {
+      map: {
+        html: 'swig',
+        md: 'hogan'
+      }
+    });
+  }
 };
 
 Config.prototype.applyOverride = function () {
-	let userHome = this.getUserHome(),
-		configPath = path.join(userHome, '.' + this.name),
-		profileOverrides;
+  let userHome = this.getUserHome(),
+    configPath = path.join(userHome, '.' + this.name),
+    profileOverrides;
 
-	try {
-		profileOverrides = require(configPath);
-		_.merge(this, profileOverrides);
-		console.log('Applied config override in %s.', configPath);
-	} catch (err) {
-		console.log('No config override found in %s.', configPath);
-	}
+  try {
+    profileOverrides = require(configPath);
+    _.merge(this, profileOverrides);
+    console.log('Applied config override in %s.', configPath);
+  } catch (err) {
+    console.log('No config override found in %s.', configPath);
+  }
 };
 
 Config.prototype.getUserHome = function () {
-	return process.env.HOME || 
-		process.env.HOMEPATH || 
-		process.env.USERPROFILE;
+  return process.env.HOME || 
+    process.env.HOMEPATH || 
+    process.env.USERPROFILE;
 };
 
 
